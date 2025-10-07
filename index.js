3f16e00939e42ca50ddfd899b9fa9d9020b0b3df
Jean Paul Ferreira <jeanpaul@jepafe.net>
2025-10-07 20:57:23 -0300

    GET products/{id}

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ let [, , method, resource] = process.argv;
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
+// products/5
+if (method == "GET" && resource.startsWith("products/")) {
+  //   const [, id] = resource.split("/");
+  let id = resource.split("/")[1];
+  id = parseInt(id);
+
+  if (isNaN(id) || id <= 0) {
+    console.log("No es un numero");
+  }
+
+  fetch("https://fakestoreapi.com/products/" + id)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+// const match = argv.find((arg) => /^products\/\d+$/.test(arg));
+// const id = match ? match.split("/")[1] : null;
+
+// if (method == "GET" && id) {}
+
 if (method == "GET" && resource == "products") {
   //   fetch("https://fakestoreapi.com/products")
   //     .then((response) => response.json())
